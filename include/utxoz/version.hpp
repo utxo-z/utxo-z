024ae5c19cbc2a30e1885077ed5f854292357627
Fernando Pelliccioni <fpelliccioni@gmail.com>
2026-02-14 22:41:49 +0100

    ci: Add release workflow and scripts (#14)
    - Extend ci.yml with release/hotfix branch triggers and version tags
    - Add setup job with determine-version action
    - Use dynamic version instead of hardcoded 0.0.0-ci
    - Add release.sh, post-release.sh, rollback-release.sh scripts
    - Add determine-version composite action

---
+++ include/utxoz/version.hpp
@@ -0,0 +1,19 @@
+// Copyright (c) 2016-present Knuth Project developers.
+// Distributed under the MIT software license, see the accompanying
+// file COPYING or http://www.opensource.org/licenses/mit-license.php.
+
+///////////////////////////////////////////////////////////////////////////////
+//
+//        GENERATED SOURCE CODE, DO NOT EDIT EXCEPT EXPERIMENTALLY
+//
+///////////////////////////////////////////////////////////////////////////////
+
+#pragma once
+
+#include <string_view>
+
+namespace utxoz {
+
+inline constexpr std::string_view version = "0.0.0-dev";
+
+} // namespace utxoz
